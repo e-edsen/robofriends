71f1165244ec0284a3b31e6a21ec1dc5e09b7224
edsen <edy.setiawan213@gmail.com>
2021-10-07 02:02:05 +0800

    little update and deployed

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
     }
 
     componentDidMount(){
+        document.title = "robofriends"
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users =>this.setState({ robots: users}))
@@ -35,7 +37,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundry> 
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         );
